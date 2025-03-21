c0083c035d261387a313ae07317aaa305dfe58f2
Gobi Mahato <116241775+gobiweb@users.noreply.github.com>
2025-03-21 20:30:02 +0530

    1. fancy-counter

---
+++ fancy-counter/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+import "./index.css";
+
+function App() {
+  return (
+    <main>
+      <Card />
+    </main>
+  );
+}
+
+export default App;
---
+++ fancy-counter/src/components/ButtonContainer.jsx
@@ -0,0 +1,4 @@
+
+export default function ButtonContainer({ children }) {
+  return <div className="button-container">{children}</div>;
+}
